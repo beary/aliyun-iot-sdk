2a8c48b162ad34cb308081627fb23330b5df720f
beary <bearyme@outlook.com>
2021-01-18 16:55:12 +0800

    0.1.1

--- src/types.ts
+++ src/types.ts
@@ -26,25 +26,18 @@ export interface CommonRequestParameters {
   RegionId: RegionId
 }
 
-export type RequestParameters = CreateProductParameters |
-  PubParameters | RRpcParameters | PubBroadcastParameters |
+export type RequestParameters = PubParameters | PubBroadcastParameters | RRpcParameters |
   GetDeviceShadowParameters | UpdateDeviceShadowParameters
 
-export interface CreateProductParameters {
-  Action: 'CreateProduct'
-  ProductName: string
-  NodeType: 0 | 1
-  AliyunCommodityCode?: 'iothub_senior' | 'iothub'
-  DataFormat: 0 | 1
-  Description?: string
-  Id2?: boolean
-  ProtocolType?: 'modbus' | 'opc-ua' | 'customize' | 'ble' | 'zigbee'
-  NetType?: 'WIFI' | 'CELLULAR' | 'ETHERNET' | 'LORA' | 'OTHER'
-  JoinPermissionId?: string
-}
-
+/** 通过自定义Topic向指定设备发布消息 */
 export interface PubParameters {
   Action: 'Pub'
+  /**
+   * 要发送的消息主体
+   *
+   * 您需要将消息原文转换成二进制数据，并进行Base64编码，从而生成消息主体
+   */
+  MessageContent: string
   /** 要发送消息的产品 Key */
   ProductKey: string
   /**
@@ -56,28 +49,34 @@ export interface PubParameters {
    * * 指定Topic的操作权限须为发布或发布和订阅
    */
   TopicFullName: string
+  /** 实例ID。公共实例不传此参数，企业版实例需传入 */
+  IotInstanceId?: string
+  /**
+   * 指定消息的发送方式。取值：
+   * 0：最多发送一次。
+   * 1：最少发送一次。
+   * 如果不传入此参数，则使用默认值0
+   */
+  Qos?: 0 | 1
+  /** 使用MQTT 5.0版本通信协议时，请求和响应模式中的响应主题，即通信的Topic */
+  ResponseTopic?: string
+  /** 使用MQTT 5.0版本通信协议时，请求和响应模式中的相关数据，用户可自定义 */
+  CorrelationData?: string
+}
+
+/** 向指定产品所有设备，或向订阅了指定Topic的所有设备发布广播消息 */
+export interface PubBroadcastParameters {
+  Action: 'PubBroadcast'
   /**
    * 要发送的消息主体
    *
    * 您需要将消息原文转换成二进制数据，并进行Base64编码，从而生成消息主体
    */
   MessageContent: string
-  Qos?: 0 | 1
-}
-
-export interface RRpcParameters {
-  Action: 'RRpc'
-  ProductKey: string
-  DeviceName: string
-  RequestBase64Byte: string
-  Timeout: number
-  Topic?: string
-}
-
-export interface PubBroadcastParameters {
-  Action: 'PubBroadcast'
   /** 要发送广播消息的产品 Key */
   ProductKey: string
+  /** 实例ID。公共实例不传此参数，企业版实例需传入 */
+  IotInstanceId?: string
   /**
    * 要接收广播消息的Topic全称
    *
@@ -89,12 +88,27 @@ export interface PubBroadcastParameters {
 如果您的设备超过数量限制，您可以对设备进行分组。例如，如果您有5,000个设备，您可以将设备按每组1,000个，而分成5组。您需要分5次调用广播Topic，自定义字段分别设置为group1/2/3/4/5，然后让每组设备分别订阅各自分组的广播Topic
    */
   TopicFullName: string
+}
+
+/** 向指定设备发送请求消息，并同步返回响应 */
+export interface RRpcParameters {
+  Action: 'RRpc'
+  /** 要接收消息的设备名称 */
+  DeviceName: string
+  /** 要发送消息产品的 ProductKey */
+  ProductKey: string
+  /** 要发送的消息内容经过Base64编码得到的字符串格式数据 */
+  RequestBase64Byte: string
+  /** 等待设备回复消息的时间，单位是毫秒，取值范围是 1000 ~ 8000 */
+  Timeout: number
+  /** 实例ID。公共实例不传此参数，企业版实例需传入 */
+  IotInstanceId?: string
   /**
-   * 要发送的消息主体
+   * 使用自定义的RRPC相关Topic。需要设备端配合使用，请参见设备端开发自定义Topic。
    *
-   * 您需要将消息原文转换成二进制数据，并进行Base64编码，从而生成消息主体
+   * 不传入此参数，则使用系统默认的RRPC Topic
    */
-  MessageContent: string
+  Topic?: string
 }
 
 export interface GetDeviceShadowParameters {
